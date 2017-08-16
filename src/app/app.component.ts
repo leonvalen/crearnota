71b0549fbd3b2980f1bdcdfea9a0ea613045e2c5
Jose Leonardo Valenzuela <leonvalen@gmail.com>
2017-08-15 22:46:33 -0300

    Instación de Bootstrap, impor-bootstrap, Listado de Notas desde arreglo, mostrar y ocultar crear notas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name = '';
+
+  my_notes = [
+    { id: 1, title: 'Nota 1', description: 'Descripción para la nota 1' },
+    { id: 2, title: 'Nota 2', description: 'Descripción para la nota 2' },
+    { id: 3, title: 'Nota 3', description: 'Descripción para la nota 3' },
+    { id: 4, title: 'Nota 4', description: 'Descripción para la nota 4' },
+    { id: 5, title: 'Nota 5', description: 'Descripción para la nota 5' }
+  ];
+  note = {};
+
+  show_form = false;
+  addNote() {
+    this.show_form = true;
+  }
+  cancel() {
+    this.show_form = false;
+  }
+
 }
